3cdfac24046f3e73581547d4315a6fb9c685a99e
William F. Broderick <billbrod@gmail.com>
2018-01-25 11:05:46 -0500

    cleans up blank spaces, unnecessary import

--- preprocessing/prisma_preproc.py
+++ preprocessing/prisma_preproc.py
@@ -2,7 +2,6 @@ import argparse
 import sys
 import os
 import os.path as op
-import shutil
 from glob import glob
 import numpy as np
 from nipype import Workflow, Node, MapNode, DataSink
@@ -98,7 +97,7 @@ def main(arglist):
     session['plugin_args'] = {}
     if args['plugin_args'] is not None:
         for arg in args['plugin_args'].split(','):
-            if len(arg.split(':'))!=2:
+            if len(arg.split(':')) != 2:
                 raise Exception("Your plugin_args is incorrectly formatted, each should contain one colon!")
             k, v = arg.split(':')
             try:
@@ -121,13 +120,11 @@ def main(arglist):
     preproc_wf.run(session["plugin"], plugin_args=session['plugin_args'])
 
 
-
 def create_preproc_workflow(session):
-    
     """
     Defines simple functional preprocessing workflow, including motion
-    correction, registration to distortion scans, and unwarping. Assumes 
-    recon-all has been performed on T1, and computes but does not apply 
+    correction, registration to distortion scans, and unwarping. Assumes
+    recon-all has been performed on T1, and computes but does not apply
     registration to anatomy.
     """
 
@@ -138,9 +135,9 @@ def create_preproc_workflow(session):
     #---EPI Realignment---
 
     # Realign every TR in each functional run to the sbref image using mcflirt
-    realign = MapNode(fsl.MCFLIRT(ref_file=session['sbref'], 
-                                  save_mats=True, 
-                                  save_plots=True), 
+    realign = MapNode(fsl.MCFLIRT(ref_file=session['sbref'],
+                                  save_mats=True,
+                                  save_plots=True),
                       iterfield=['in_file'], name='realign')
     realign.inputs.in_file = session['epis']
     wf.add_nodes([realign])
@@ -149,11 +146,11 @@ def create_preproc_workflow(session):
     #---Registration to distortion scan---
 
     # Register the sbref scan to the distortion scan with the same PE using flirt
-    reg2dist = Node(fsl.FLIRT(in_file=session['sbref'], 
-                              reference=session['distort_PE'], 
+    reg2dist = Node(fsl.FLIRT(in_file=session['sbref'],
+                              reference=session['distort_PE'],
                               out_file='sbref_reg.nii.gz',
-                              out_matrix_file='sbref2dist.mat', 
-                              dof=6), 
+                              out_matrix_file='sbref2dist.mat',
+                              dof=6),
                     name='reg2distort')
     wf.add_nodes([reg2dist])
 
@@ -162,8 +159,8 @@ def create_preproc_workflow(session):
 
     # Merge the two distortion scans for unwarping
     distort_scans = [session['distort_PE'], session['distort_revPE']]
-    merge_dist = Node(fsl.Merge(in_files=distort_scans, 
-                                dimension='t', 
+    merge_dist = Node(fsl.Merge(in_files=distort_scans,
+                                dimension='t',
                                 merged_file='distortion_merged.nii.gz'),
                       name='merge_distort')
     wf.add_nodes([merge_dist])
@@ -171,7 +168,7 @@ def create_preproc_workflow(session):
     # Run topup to estimate warpfield and create unwarped distortion scans
     PEs = np.repeat([session['PE_dim'], session['PE_dim'] + '-'], 3)
     unwarp_dist = Node(fsl.TOPUP(encoding_direction=list(PEs),
-                                 readout_times=[1, 1, 1, 1, 1, 1], 
+                                 readout_times=[1, 1, 1, 1, 1, 1],
                                  config='b02b0.cnf'),
                        name='unwarp_distort')
     wf.connect(merge_dist, 'merged_file', unwarp_dist, 'in_file')
@@ -179,9 +176,9 @@ def create_preproc_workflow(session):
     # Unwarp sbref image in case it's useful
     unwarp_sbref = Node(fsl.ApplyTOPUP(in_index=[1], method='jac'),
                         name='unwarp_sbref')
-    wf.connect([(reg2dist, unwarp_sbref, 
-                    [('out_file', 'in_files')]),
-                (unwarp_dist, unwarp_sbref, 
+    wf.connect([(reg2dist, unwarp_sbref,
+                 [('out_file', 'in_files')]),
+                (unwarp_dist, unwarp_sbref,
                     [('out_enc_file', 'encoding_file'),
                      ('out_fieldcoef', 'in_topup_fieldcoef'),
                      ('out_movpar', 'in_topup_movpar')])])
@@ -190,15 +187,15 @@ def create_preproc_workflow(session):
     #---Registration to anatomy---
 
     # Create mean unwarped distortion scan
-    mean_unwarped_dist = Node(fsl.MeanImage(dimension='T'), 
+    mean_unwarped_dist = Node(fsl.MeanImage(dimension='T'),
                               name='mean_unwarped_distort')
     wf.connect(unwarp_dist, 'out_corrected', mean_unwarped_dist, 'in_file')
 
     # Register mean unwarped distortion scan to anatomy using bbregister
-    reg2anat = Node(fs.BBRegister(subject_id=session['subj'], 
-                                  contrast_type='t2', 
-                                  init='fsl', 
-                                  out_reg_file='distort2anat_tkreg.dat', 
+    reg2anat = Node(fs.BBRegister(subject_id=session['subj'],
+                                  contrast_type='t2',
+                                  init='fsl',
+                                  out_reg_file='distort2anat_tkreg.dat',
                                   out_fsl_file='distort2anat_flirt.mat'),
                     name='reg2anat')
     wf.connect(mean_unwarped_dist, 'out_file', reg2anat, 'source_file')
@@ -207,38 +204,38 @@ def create_preproc_workflow(session):
     #---Combine and apply transforms to EPIs---
 
     # Split EPI runs into 3D files
-    split_epis = MapNode(fsl.Split(dimension='t'), 
+    split_epis = MapNode(fsl.Split(dimension='t'),
                          iterfield=['in_file'], name='split_epis')
     split_epis.inputs.in_file = session['epis']
     wf.add_nodes([split_epis])
 
     # Combine the rigid transforms to be applied to each EPI volume
     concat_rigids = MapNode(fsl.ConvertXFM(concat_xfm=True),
-                            iterfield=['in_file'], 
+                            iterfield=['in_file'],
                             nested=True,
                             name='concat_rigids')
-    wf.connect([(realign, concat_rigids, 
-                    [('mat_file', 'in_file')]),
-                (reg2dist, concat_rigids, 
+    wf.connect([(realign, concat_rigids,
+                 [('mat_file', 'in_file')]),
+                (reg2dist, concat_rigids,
                     [('out_matrix_file', 'in_file2')])])
 
     # Apply rigid transforms and warpfield to each EPI volume
     correct_epis = MapNode(fsl.ApplyWarp(interp='spline', relwarp=True),
-                           iterfield=['in_file', 'ref_file', 'premat'], 
-                           nested=True, 
+                           iterfield=['in_file', 'ref_file', 'premat'],
+                           nested=True,
                            name='correct_epis')
 
     get_warp = lambda warpfields: warpfields[0]
-    wf.connect([(split_epis, correct_epis, 
+    wf.connect([(split_epis, correct_epis,
                     [('out_files', 'in_file'),
                      ('out_files', 'ref_file')]),
-                (concat_rigids, correct_epis, 
+                (concat_rigids, correct_epis,
                     [('out_file', 'premat')]),
-                (unwarp_dist, correct_epis, 
+                (unwarp_dist, correct_epis,
                     [(('out_warps', get_warp), 'field_file')])])
 
     # Merge processed files back into 4D nifti
-    merge_epis = MapNode(fsl.Merge(dimension='t', 
+    merge_epis = MapNode(fsl.Merge(dimension='t',
                                    merged_file='timeseries_corrected.nii.gz'),
                          iterfield='in_files',
                          name='merge_epis')
@@ -246,8 +243,8 @@ def create_preproc_workflow(session):
 
 
     #---Copy important files to main directory---
-    substitutions = [('_merge_epis%d/timeseries_corrected.nii.gz' %r, 
-                      'timeseries_corrected_run%02d.nii.gz' %(r+1))
+    substitutions = [('_merge_epis%d/timeseries_corrected.nii.gz' % r,
+                      'timeseries_corrected_run%02d.nii.gz' % (r+1))
                      for r in np.arange(len(session['epis']))]
     ds = Node(DataSink(base_directory=os.path.abspath(session['out']),
                        substitutions=substitutions),
@@ -259,7 +256,7 @@ def create_preproc_workflow(session):
     wf.connect(merge_epis, 'merged_file', ds, '@merge_epis')
 
     return wf
-    
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
