15b6850595020c8c67c37dd9a86bf2895bc024b2
elinekupers <elinekupers@gmail.com>
2020-02-27 17:55:34 -0500

    separate get stimulus from bids analyze prf, since it is used by other functions now as well

---
+++ BIDS/getStimulus.m
@@ -0,0 +1,36 @@
+function [stimulus, stimwidthpix] = getStimulus(aperturePath, tasks, runnums)
+% <stimulus> provides the apertures as a cell vector of R x C x time.
+%   values should be in [0,1].  the number of time points can differ across runs.
+
+stimFiles = dir(fullfile(aperturePath, '*aperture.mat'));
+
+stimNames = {stimFiles.name};
+
+scan = 1;
+
+for ii = 1:length(tasks)
+    for jj = 1:length(runnums{ii})
+        
+        % check for both 0-padded and non-0-padded integers, throw
+        % an error if we find other than 1.
+        prfIdx = contains(stimNames,sprintf('task-%s_run-%d_',...
+            tasks{ii}, runnums{ii}(jj)));
+        prfIdx = or(prfIdx, contains(stimNames,sprintf('task-%s_run-%02d_',...
+            tasks{ii}, runnums{ii}(jj))));
+        if sum(prfIdx) > 1
+           error(['Found more than one stim file for task %s, run %d / '...
+                  '%02d'], tasks{ii}, runnums{ii}(jj), runnums{ii}(jj))
+        elseif sum(prfIdx) == 0 
+            error (['Stim file *_task-%s_run-%d_aperture.mat'...
+                    ' was not found'],tasks{ii}, runnums{ii}(jj))
+        else
+            tmp = load(fullfile(stimFiles(prfIdx).folder,stimFiles(prfIdx).name), 'stimulus');
+            stimulus{scan} = tmp.stimulus;
+            scan         = scan+1;
+        end
+    end
+    
+    stimwidthpix = size(stimulus{1},2);
+
+end
+end
